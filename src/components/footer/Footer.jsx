a3f65baa050cd9adcf6f3e8e5a0563bb54cc8dac
dinesh07sengar <sengardante@gmail.com>
2023-05-18 15:00:57 +0530

    third

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -13,15 +13,15 @@ const Footer = () => {
   };
   
   return (
-    <footer className="footer" >
+    <footer className="footer" id="nav-menu">
       <a href="#home" className="footer__logo">Dinesh-sengar</a>
       <ul className="nav-menu">
-        <li><a href="#home" className="nav-link home1">Home</a></li>
-        <li><a href="#about" className="nav-link about1">About</a></li>
-        <li><a href="#skills" className="nav-link skills1">Skills</a></li>
-        <li><a href="#projects" className="nav-link projects1">Projects</a></li>
-        <li><a href="#home" className="nav-link resume1" id="resume-link-1">Resume</a></li>
-        <li><a href="#contact" className="nav-link projects1">Contact</a></li>
+        <li><a href="#home" className="nav-link home">Home</a></li>
+        <li><a href="#about" className="nav-link about">About</a></li>
+        <li><a href="#skills" className="nav-link skills">Skills</a></li>
+        <li><a href="#projects" className="nav-link projects">Projects</a></li>
+        <li><a href={CV} download className="nav-link resume" id="resume-button-1">Resume</a></li>
+        <li><a href="#contact" className="nav-link contact">Contact</a></li>
       </ul>
       <div className="footer__socials">
       <a href="https://www.linkedin.com/in/dinesh-sengar-199a98201/" target="_blank" rel="noreferrer" ><BsLinkedin /></a>
